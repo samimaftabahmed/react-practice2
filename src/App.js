a8667bbe2098cd61bc0edbb8aa0fc1241846c533
Samim <samimaftabahmed@gmail.com>
2020-05-14 20:48:26 +0530

    Refactoring

--- src/App.js
+++ src/App.js
@@ -5,26 +5,32 @@ import FirstPractice from "./components/FirstPractice";
 
 export default function App() {
 
+    const data = [
+        {
+            name: "Machine Learning",
+            imgUrl: require("./images/img-1.jpg"),
+            phone: "011-6512131",
+            email: "sajhsfd@gjhbd.com"
+        },
+        {
+            name: "Big Data",
+            imgUrl: require("./images/img-2.jpg"),
+            phone: "022-54120",
+            email: "dgdfg@bnbn.com"
+        },
+        {
+            name: "Blockchain",
+            imgUrl: require("./images/img-3.jpg"),
+            phone: "033-8732233",
+            email: "iopiop@asas.com"
+        }
+    ];
+
     return (
         <div>
-            <FirstPractice contact={{
-                name: "Machine Learning",
-                imgUrl: require("./images/img-1.jpg"),
-                phone: "011-6512131",
-                email: "sajhsfd@gjhbd.com"
-            }}/>
-            <FirstPractice contact={{
-                name: "Big Data",
-                imgUrl:  require("./images/img-2.jpg"),
-                phone: "022-54120",
-                email: "dgdfg@bnbn.com"
-            }}/>
-            <FirstPractice contact={{
-                name: "Blockchain",
-                imgUrl: require("./images/img-3.jpg"),
-                phone: "033-8732233",
-                email: "iopiop@asas.com"
-            }}/>
+            <FirstPractice contact={data[0]}/>
+            <FirstPractice contact={data[1]}/>
+            <FirstPractice contact={data[2]}/>
         </div>
 
     );
